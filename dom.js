e0c17ca430e93c75fbc030817fa353185b385dd0
maibarra0904 <mario.ibarra.86@gmail.com>
2022-08-30 22:13:12 -0500

    Cambios 300822-1

--- dom.js
+++ dom.js
@@ -2,3 +2,8 @@ let texto = "Esta es una prueba del dom";
 const hablar = (texto) => speechSynthesis.speak(new SpeechSynthesisUtterance(texto));
 
 hablar(texto);
+
+
+console.log(document.querySelector("#menu"))
+console.log(document.querySelectorAll("#menu"))
+document.querySelectorAll("a").forEach((el) => console.log(el))
