232a016d1498bc36dd4c5b61d3a3aea1e6944809
maibarra0904 <mario.ibarra.86@gmail.com>
2022-09-21 17:29:45 -0400

    Cambios 210922

---
+++ Ejemplo1.js
@@ -0,0 +1,7 @@
+import hamburgerMenu from "./Menu_Ejemplo1.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded", () => {
+        hamburgerMenu(".panel-btn", ".panel",".menu a");
+    })  
