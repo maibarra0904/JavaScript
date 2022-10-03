7470ca6aa1da5e1c87be3021db8aaa42faba53be
maibarra0904 <mario.ibarra.86@gmail.com>
2022-10-03 17:45:29 -0500

    Cambios 031022

--- El ahorcado.js
+++ El ahorcado.js
@@ -1,31 +1,54 @@
 const d = document;
-let contador = 0;
+let contador = 0, $secret;
 const imageC = d.getElementById("imagen"), textBottom = d.getElementById("Registrar"),
-textBox = d.getElementById("textBlock"), varText = d.getElementById("varText");
+textBox = d.getElementById("textBlock"), varText = d.getElementById("varText"), secretText = d.getElementById("secretWord");
 
 
 
 d.addEventListener("click", (e) => {
-    
-    let $secret;
    
-    if(e.target.matches("#Registrar"))
-    {
-        if(textBox.value !== ""){
-            varText.textContent = "Realice un intento:";
-            $secret = textBox.value;
-            textBottom.textContent = "Probar";
-            textBox.value = "";
-        };
-        conteo_regresivo();
-    };
-
     if(e.target.matches("#Probar"))
     {
-        contador++;
+        clearInterval(countdownTempo);
+        intento();
+        
+        conteo_regresivo();
+        
     }
+    
+    if(e.target.matches("#Registrar"))
+    {
+        clearInterval(countdownTempo);
+        accion_inicial();
+        conteo_regresivo();
+        
+    };
+    
+    
 });
 
+let countdownTempo;
+const dinamicD = [];
+
+function accion_inicial() {
+    if(textBox.value !== ""){
+        varText.textContent = "Realice un intento:";
+        $secret = textBox.value;
+        deletreo = $secret.split("");
+        textBottom.textContent = "Probar";
+        textBottom.setAttribute("id","Probar");
+        textBox.value = "";
+        textBox.setAttribute("placeholder","");
+        textBox.setAttribute("maxlength","1");
+
+        for(let i = 0; i < deletreo.length; i++) {
+            dinamicD[i] = "x";
+        
+        }
+        secretText.textContent = dinamicD;
+
+    };
+}
 
 
 function conteo_regresivo(){
@@ -34,7 +57,7 @@ function conteo_regresivo(){
     let stopTime = CountdownDate.getTime();
     const $countdown = d.getElementById("message2");
 
-    let countdownTempo = setInterval(() => {
+    countdownTempo = setInterval(() => {
         let now = new Date().getTime(),
         efectiveTime = now - stopTime,
         regressiveTime = 10 - Math.round(efectiveTime/1000);        ;
@@ -49,8 +72,60 @@ function conteo_regresivo(){
         }
         
     },1000);
-
     
+    
+}
 
+let deletreo, $num =  0;
+
+function intento() {
+    for(let i=0; i<deletreo.length; i++) {
+        if(deletreo[i]===textBox.value) {
+            dinamicD[i]=deletreo[i];
+            $num++;
+            
+        }
+    }
+    
+    
+    if(deletreo.toLocaleString() === dinamicD.toLocaleString()) {
+        alert('Ganaste');
+        location.reload();
+    }
+
+    if($num===0) {
+        contador++;
+        switch (contador) {
+            case 1:
+                imageC.setAttribute('src', '/ahorcado/ahorcado1.png');
+                varText.textContent = "Llevas 1 intento de 5, prueba con otra letra:";
+                break;
+            case 2:
+                imageC.setAttribute('src', '/ahorcado/ahorcado2.png');
+                varText.textContent = "Llevas 2 intentos de 5, prueba con otra letra:";
+                break;
+            case 3:
+                imageC.setAttribute('src', '/ahorcado/ahorcado3.png');
+                varText.textContent = "Llevas 3 intentos de 5, prueba con otra letra:";
+                break;
+            case 4:
+                imageC.setAttribute('src', '/ahorcado/ahorcado4.png');
+                varText.textContent = "Llevas 4 intentos de 5, prueba con otra letra:";
+                break;
+
+            case 5:
+                alert('Perdiste');
+                location.reload();
+            default:
+                break;
+        }
+    }
+    
+    
+    
+    
+    $num=0;
+    textBox.value = "";
+    secretText.textContent = dinamicD;
     
 }
