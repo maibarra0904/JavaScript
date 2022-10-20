41396fcef45174f12a81c41df92067f24a4e2610
maibarra0904 <mario.ibarra.86@gmail.com>
2022-10-20 09:27:24 -0500

    Cambios 201022

--- Ejemplo2.js
+++ Ejemplo2.js
@@ -21,13 +21,13 @@ d.addEventListener("DOMContentLoaded", () => {
         hamburgerMenu(".panel-btn", ".panel",".menu a");
         digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
         alarm("sonidos/alarma.mp3","#activar-alarma","#desactivar-alarma");
-        Countdown("Countdown","Apr 09, 2023 00:00:00","Felicidades");
+        //Countdown("Countdown","Apr 09, 2023 00:00:00","Felicidades"); //Ejemplo 4
         responsiveMedia("Youtube", "(min-width: 768px)",`<a href="https://www.youtube.com/watch?v=u9IMgIT1A6M" target="_blank"
         rel="noopener">Ver Video</a>`, `<iframe width="560" height="315" src="https://www.youtube.com/embed/u9IMgIT1A6M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);
         responsiveMedia("Map", "(min-width: 768px)",`<a href="https://goo.gl/maps/bzuPA1mCfpqCkLV97" target="_blank"
         rel="noopener">Ver Mapa</a>`, `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.797848403771!2d-78.47328118475889!3d-0.18354423547017534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d591ecf0995281%3A0xcf40cb5e608498b3!2sCentro%20del%20mundo%20(Ecuador)!5e0!3m2!1ses-419!2sec!4v1664912704324!5m2!1ses-419!2sec" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`);
         responsiveTester("Responsive tester");
-        //userDevicelnfo("user-device"); - Ejemplo 8
+        //userDevicelnfo("user-device"); // Ejemplo 8
                 
     });
 
