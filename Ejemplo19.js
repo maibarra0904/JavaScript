a0dd28e24bf4afea461799827c8416102df1fb5a
maibarra0904 <mario.ibarra.86@gmail.com>
2022-10-31 21:57:07 -0500

    Cambios311022

---
+++ Ejemplo19.js
@@ -0,0 +1,54 @@
+const $axios = document.getElementById("axios"),
+    $fragment = document.createDocumentFragment(),
+    input = document.querySelector ("#searchInput");
+
+let json;
+
+(() => {
+    
+    const $loader = document.querySelector(".loader");
+
+    axios.get("https://dummyjson.com/users")
+    .then(
+        (res) => {
+
+            console.log(res);
+
+            json = res.data.users;
+
+            json.forEach( (el) => {
+                const $li = document.createElement("li");
+                $li.innerHTML = `${el.firstName} ${el.lastName}`;
+                $fragment.appendChild($li);
+
+            })
+
+            
+            
+            $axios.appendChild($fragment);
+            $loader.classList.add("none");
+        }
+    )
+    .catch(
+
+    ).finally(
+    );
+}
+
+)();
+
+input.addEventListener("keyup" , (e) => {
+
+    const $newUsers = json.filter(user => `${user.firstName.toLowerCase()}${user.lastName.toLowerCase()}`
+    .includes(input.value.toLowerCase()))
+    
+    renderUsers($newUsers)
+})
+
+
+
+function renderUsers (users) {
+    
+    const createUserItems = users => users.map(user => `<li>${user.firstName} ${user.lastName}</li>`).join(" ")
+    $axios.innerHTML = createUserItems(users);
+}
