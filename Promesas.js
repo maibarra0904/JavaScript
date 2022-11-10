e97e144a4183fd29b134cf61f2ddc55ab585b2f0
maibarra0904 <mario.ibarra.86@gmail.com>
2022-11-10 10:08:15 -0500

    Cambios101122

--- Promesas.js
+++ Promesas.js
@@ -1,22 +1,35 @@
 // Diferencia del funcionamiento de una promesa sincrónica y asincrónica
 
-const tipo = "sincronico"; //registra "sincronico" o "asincronico"
+const numero = 4; //registra "1: sincronico fecth", "2: asincronico fetch", "3: sincronico axios", "4: asincronico axios"
+
+const tipo = numero === 1 ? "sincronico fetch" : numero === 2 ? "asincronico fetch" : 
+numero === 3 ? "sincronico axios" : numero === 4 ? "asincronico axios" : "";
+
+let direccion = "http://localhost:3500/santos";
+
+const $fragment = document.createDocumentFragment(),
+        $tipo = document.createElement("p"),
+        $fetch = document.getElementById("fetch"),
+        $axios = document.getElementById("axios");
+
+$tipo.innerHTML = `Modo ${tipo}`;
+$fragment.appendChild($tipo);
+
+
+
 
 (() => {
     console.log(tipo);
-    const $tipo = document.createElement("p");
-    $tipo.innerHTML = `Modo ${tipo}`;
 
-    if(tipo === "sincronico") {
-        const $fetch = document.getElementById("fetch"),
-        $fragment = document.createDocumentFragment();
-    
+    //Promesa Síncrona usando Fetch
 
-        fetch("http://localhost:3500/santos")
+    if(tipo === "sincronico fetch") {        
+
+        fetch(direccion)
         .then((res) => (res.ok ? res.json() : Promise.reject(res)))
         .then((json)=> {
             console.log(json);
-            $fragment.appendChild($tipo)
+            
             json.forEach( el => {
                 const $li = document.createElement("li");
                 $li.innerHTML = `${el.nombre}`;
@@ -32,13 +45,16 @@ const tipo = "sincronico"; //registra "sincronico" o "asincronico"
             $fetch.innerHTML = `Error ${err.status}: ${message}`;
         })
         .finally(() => console.log("Esto se ejecuta de forma independiente"));
-    } else if (tipo === "asincronico") {
+    } 
+    
+    //Promesa Asíncrona usando Fetch
+
+    else if (tipo === "asincronico fetch") {
         const getAll = async() => {
             try{
-                const $fetch = document.getElementById("fetch"),
-                $fragment = document.createDocumentFragment();
-                $fragment.appendChild($tipo);
-                let res = await fetch("http://localhost:3500/santos"),
+                
+                
+                let res = await fetch(direccion),
                 json = await res.json();
                 console.log(json);
                 json.forEach(el => {
@@ -56,7 +72,58 @@ const tipo = "sincronico"; //registra "sincronico" o "asincronico"
         document.addEventListener("DOMContentLoaded", getAll);
     }
 
+    //Promesa Síncrona usando Axios
+
+    else if (tipo ===  "sincronico axios"){
+        axios.get(direccion) //Se puede leer el archivo JSON guardado en el equipo
+        .then(
+            (res) => {
+
+
+                let json = res.data;
+                
+                json.forEach( (el) => {
+                    const $li = document.createElement("li");
+                    $li.innerHTML = `${el.nombre}`;
+                    $fragment.appendChild($li);
+                })
+
+                $axios.appendChild($fragment);
+                
+            }
+        )
+        .catch(
+
+        ).finally(
+        );
+    }
+
+    //Promesa Asíncrona usando Axios
+
+    else if (tipo ===  "asincronico axios") {
+        const getAll = async () => {
+            try {
+                let res = await axios.get(direccion),
+                json = await res.data;
+        
+                json.forEach(el => {
+                    const $li = document.createElement("li");
+                    $li.innerHTML = `${el.nombre}`;
+                    $fragment.appendChild($li);
+                })
+                
+                $axios.appendChild($fragment);
+                
+            } catch (error) {
+                let message = `Ocurrió un error: ${error}`;
+            }
+        }
+        
+        
+        document.addEventListener("DOMContentLoaded",getAll);
+    }
+
     
     
-})()
+})();
 
