bc527e3b459d802f2cb58b36e428aa99d140c61a
maibarra0904 <mario.ibarra.86@gmail.com>
2022-12-12 08:47:36 -0500

    Cambios121222

--- spa/app/components/Router.js
+++ spa/app/components/Router.js
@@ -2,6 +2,7 @@ import { ajax } from "../helpers/ajax.js";
 import api from "../helpers/wp_api.js";
 import { Post } from "./Post.js";
 import { PostCard } from "./PostCard.js";
+import { SearchCard } from "./SearchCard.js";
 
 
 export async function Router() {
@@ -28,7 +29,36 @@ export async function Router() {
             }
         })
     } else if (hash.includes("#/search")) {
-        $main.innerHTML = `<h2>Seccion del Buscador</h2>`;
+        let query = localStorage.getItem("wpSearch");
+
+        if(!query) {
+            d.querySelector(".loader").style.display = "none";
+            return false
+        };
+
+        await ajax ({
+        
+            
+            url: `${api.SEARCH}${query}`,
+            cbSuccess: (search) => {
+                console.log(search);
+                let html = "";
+
+                if(search.length === 0) {
+                    html = `
+                        <p class = "error">
+                            No existen resultados de busqueda para el termino <mark>${query}</mark>
+                        </p>
+                    `
+                } else {
+                    search.forEach((post) => (html+=SearchCard(post)));
+                };
+                $main.innerHTML = html;
+                
+            }
+        })
+        
+        //$main.innerHTML = `<h2>Seccion del Buscador</h2>`;
         //d.querySelector(".loader").style.display = "none";
     } else if (hash === "#/contact") {
         $main.innerHTML = `<h2>Seccion de Contacto</h2>`;
@@ -37,6 +67,8 @@ export async function Router() {
         $main.innerHTML = `<h2>Aqui cargara el elemento del post previamente seleccionado</h2>`;
         //d.querySelector(".loader").style.display = "none";
         await ajax ({
+        
+            
             url: `${api.POST}/${localStorage.getItem("wpPostId")}`,
             cbSuccess: (post) => {
                 console.log(post);
