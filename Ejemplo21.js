4c4af3410ad5ae36f0448c6b83bf40da7ab5bf9f
maibarra0904 <mario.ibarra.86@gmail.com>
2023-01-02 11:52:30 -0500

    Cambios020123-2

--- Ejemplo21.js
+++ Ejemplo21.js
@@ -8,12 +8,12 @@ $title = d.querySelector(".crud-title"), //Llamar el título
 $template = d.getElementById("crud-template").content, //Llamar el contenido del template
 $fragment = d.createDocumentFragment(); //Crear un nuevo fragmento
 
-const ajax = (options) => {
-    let {url, method, success, error, data} = options;
-    const xhr = new XMLHttpRequest();
+//Se crea la función para generar la petición AJAX
+const ajax = ({url, method, success, error, data}) => {
+    const xhr = new XMLHttpRequest(); //Se llama a la petición http
 
-    xhr.addEventListener("readystatechange", e => {
-        if(xhr.readyState !== 4 ) return;
+    xhr.addEventListener("readystatechange", e => { //Se pone a la escucha cuando hay cambios
+        if(xhr.readyState !== 4 ) return; //Se asegura tener una respuesta para continuar
 
         if(xhr.status >=200 && xhr.status < 300){
             let json = JSON.parse(xhr.responseText);
@@ -24,33 +24,32 @@ const ajax = (options) => {
         }
     });
 
-    xhr.open(method || "GET", url);
+    xhr.open(method || "GET", url); //Se toma la información de la API
 
-    xhr.setRequestHeader("Content-type","application/json; charset=utf-8");
+    xhr.setRequestHeader("Content-type","application/json; charset=utf-8"); //Se formatea el contenido
 
-    xhr.send(JSON.stringify(data));
+    xhr.send(JSON.stringify(data)); //Se envía la petición en formato JSON
 
-}
-
+}//Función para llamar a todos los datos de la BD
 const getAll = () => {
     ajax({
         method: "GET",
-        url: "http://localhost:3500/santos",
+        url: "http://localhost:3500/santos", //Debe estar activo el servidor para ejecutar esto
         success: (res) => {
-            res.forEach(el => {
-                $template.querySelector(".name").textContent = el.nombre;
-                $template.querySelector(".constellation").textContent = el.constelacion;
-                $template.querySelector(".edit").dataset.id = el.id;
-                $template.querySelector(".edit").dataset.name = el.nombre;
+            res.forEach(el => { //Por cada elemento de la base de datos
+                $template.querySelector(".name").textContent = el.nombre; //Agrega el nombre
+                $template.querySelector(".constellation").textContent = el.constelacion; //La constelación
+                $template.querySelector(".edit").dataset.id = el.id; //Agrega un botón con id
+                $template.querySelector(".edit").dataset.name = el.nombre; //Le coloca el
                 $template.querySelector(".edit").dataset.constellation = el.constelacion;
-                $template.querySelector(".delete").dataset.id = el.id;
+                $template.querySelector(".delete").dataset.id = el.id; //El botón eliminar
 
-                let $clone = d.importNode($template, true);
-                $fragment.appendChild($clone);
+                let $clone = d.importNode($template, true); //Registra la información en el template (donde se mostrará la info)
+                $fragment.appendChild($clone);//Crea un nuevo fragmento con el template anterior
             });
             console.log(res)
 
-            $table.querySelector("tbody").appendChild($fragment);
+            $table.querySelector("tbody").appendChild($fragment); //Agrega el fragmento en el html
         },
         error: (err) => {
             $table.insertAdjacentHTML("afterend",`<p><b>${err}</b></p>`)    
@@ -63,34 +62,35 @@ const getAll = () => {
     })
 }
 
-d.addEventListener("DOMContentLoaded", getAll);
+d.addEventListener("DOMContentLoaded", getAll); //Se pone a la escucha la función anterior
 
+//Configuración del botón enviar
 d.addEventListener("submit", e => {
-    if(e.target === $form) {
+    if(e.target === $form) { //Si el botón coincide con el de enviar
         e.preventDefault();
 
-        if(!e.target.id.value){
+        if(!e.target.id.value){ //Si el registro no contiene id
             //Create - POST
             ajax({
-                url: "http://localhost:3500/santos",
-                method: "POST",
+                url: "http://localhost:3500/santos", //Selecciona la API
+                method: "POST", //Crea un nuevo registro
                 success: (res) => location.reload(),
-                error: () => $form.insertAdjacentHTML("afterend",`<p><b>${err}</b></p>`),
+                error: () => $form.insertAdjacentHTML("afterend",`<p><b>${err}</b></p>`), //Se inserta en el formulario un nuevo elemento
                 data: {
-                    nombre: e.target.nombre.value,
-                    constelacion: e.target.constelacion.value
+                    nombre: e.target.nombre.value, //Se le agrega el nombre
+                    constelacion: e.target.constelacion.value //Y la constelación
                 }
             })
         } else {
             //Update - PUT
             ajax({
-                url: `http://localhost:3500/santos/${e.target.id.value}`,
-                method: "PUT",
+                url: `http://localhost:3500/santos/${e.target.id.value}`, //Se ubica el registro a través de la id
+                method: "PUT", //Sirve para modificar el registro
                 success: (res) => location.reload(),
                 error: () => $form.insertAdjacentHTML("afterend",`<p><b>${err}</b></p>`),
                 data: {
-                    nombre: e.target.nombre.value,
-                    constelacion: e.target.constelacion.value
+                    nombre: e.target.nombre.value, //Se coloca el nuevo nombre
+                    constelacion: e.target.constelacion.value //Se coloca la nueva constelación
                 }
             })
 
@@ -98,22 +98,24 @@ d.addEventListener("submit", e => {
     }
 })
 
+//Configuración del evento click
 d.addEventListener("click", e => {
+    //Si coincide con el botón editar
     if(e.target.matches(".edit")){
         //alert("Editar");
-        $title.textContent = "Editar Santo";
-        $form.nombre.value = e.target.dataset.name;
-        $form.constelacion.value = e.target.dataset.constellation;
-        $form.id.value = e.target.dataset.id;
+        $title.textContent = "Editar Santo"; //Cambia el título por "Editar Santo"
+        $form.nombre.value = e.target.dataset.name; //Coloca en la caja de nombre el nombre
+        $form.constelacion.value = e.target.dataset.constellation; //Colocan en la caja de constelación el nombre de la constelación
+        $form.id.value = e.target.dataset.id; //Actualiza el id del formulario que coincida con el id del nombre
     }
 
-    if(e.target.matches(".delete")){
-        let isDelete = confirm(`Estas seguro de eliminar el Santo ${e.target.dataset.id}`);
+    if(e.target.matches(".delete")){ //Si coincide con el botón borrar
+        let isDelete = confirm(`Estas seguro de eliminar el Santo ${e.target.dataset.id}`); //Muestra una alerta para confirmar el borrado del registro
 
-        if(isDelete){
+        if(isDelete){ //Si se confirma el borrado del registro
             ajax({
-                url: `http://localhost:3500/santos/${e.target.dataset.id}`,
-                method: "DELETE",
+                url: `http://localhost:3500/santos/${e.target.dataset.id}`, //toma el registro con esa id
+                method: "DELETE", //Bórralo
                 success: (res) => location.reload(),
                 error: () => alert(err),
             })
