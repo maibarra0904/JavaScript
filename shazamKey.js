1ac56ee2254f8baa60a6a586332fe832e0f32d9e
maibarra0904 <mario.ibarra.86@gmail.com>
2023-01-09 17:46:39 -0500

    cambios090123

---
+++ shazamKey.js
@@ -0,0 +1,7 @@
+export const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '771babd4afmsh1631015e315e6cbp1394e2jsn298d90dd2f3c',
+		'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+	}
+};
