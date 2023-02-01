ef41d66456bea5924a5f4ea39c88b42ee2eba518
maibarra0904 <mario.ibarra.86@gmail.com>
2023-02-01 10:11:13 -0500

    010223

--- Promesas.js
+++ Promesas.js
@@ -1,30 +1,31 @@
 // Diferencia del funcionamiento de una promesa sincrónica y asincrónica
 
-const numero = 4; //registra "1: sincronico fecth", "2: asincronico fetch", "3: sincronico axios", "4: asincronico axios"
+const numero = 1; //registra "1: sincronico fecth", "2: asincronico fetch", "3: sincronico axios", "4: asincronico axios"
 
 const tipo = numero === 1 ? "sincronico fetch" : numero === 2 ? "asincronico fetch" : 
 numero === 3 ? "sincronico axios" : numero === 4 ? "asincronico axios" : "";
 
-let direccion = "http://localhost:3000/santos";
+let direccion = "http://localhost:3500/santos";
 
 const $fragment = document.createDocumentFragment(),
         $tipo = document.createElement("p"),
         $fetch = document.getElementById("fetch"),
-        $axios = document.getElementById("axios");
-
-$tipo.innerHTML = `Modo ${tipo}`;
-$fragment.appendChild($tipo);
+        $axios = document.getElementById("axios"),
+        boton = document.querySelector('#boton');
 
 
 
 
-(() => {
-    console.log(tipo);
-
-    //Promesa Síncrona usando Fetch
+const promesa = async function () {
+    $tipo.innerHTML = `Modo ${tipo}`;
+    $fragment.appendChild($tipo);
 
     if(tipo === "sincronico fetch") {        
 
+        //Las promesas tienen 3 valores:
+        //Pending: No se ha cumplido ni rechazado (A la espera de alguna orden)
+        //Fulfilled: Se cumplió la promesa
+        //Rejected: Se ha rechazado o no se pudo cumplir la promesa
         fetch(direccion)
         .then((res) => (res.ok ? res.json() : Promise.reject(res)))
         .then((json)=> {
@@ -68,8 +69,7 @@ $fragment.appendChild($tipo);
                 let message = err.statusText || "Ocurrio un error";
                 $fetch.innerHTML = `Error ${err.status}: ${message}`}
         }
-
-        document.addEventListener("DOMContentLoaded", getAll);
+        getAll();
     }
 
     //Promesa Síncrona usando Axios
@@ -119,11 +119,76 @@ $fragment.appendChild($tipo);
             }
         }
         
-        
-        document.addEventListener("DOMContentLoaded",getAll);
+        getAll();
     }
 
-    
-    
-})();
+}
 
+
+boton.addEventListener('click', async () => {
+    try {
+        Notification.requestPermission()
+            .then(resultado => console.log(`El resultado es: ${resultado}`));
+
+        const proAsync = await promesa();
+        try {proAsync()}
+        catch (err) {
+        };
+        
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+
+if (Notification.permission === "granted") {
+    new Notification("Título de la notificación", {
+        icon: 'imagenes/icono.jpg',
+        body: 'Este es la notificación',
+    });
+    
+  } else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      if (permission === "granted") {
+        let notification = new Notification("Título de la notificación", {
+            icon: 'imagenes/icono.jpg',
+            body: 'Este es la notificación',
+        });
+      }
+    });
+  }
+
+
+//Como ejecutar más de una promesa al mismo tiempo
+
+function descargarPedidos() {
+return new Promise(resolve => {
+    console.log('Descargando pedidos... espere');
+
+    setTimeout(() => {
+        resolve('Pedidos Descargados')
+    },1000)
+})
+}
+
+function descargarClientes() {
+return new Promise(resolve => {
+    console.log('Descargando clientes... espere');
+
+    setTimeout(() => {
+        resolve('Clientes Descargados')
+    },3000)
+})
+}
+
+async function app() {
+try {
+    const resultado = await Promise.all([descargarPedidos(),descargarClientes()]);
+    console.log(resultado[0]);
+    console.log(resultado[1]);
+} catch (err) {
+    console.log(err);
+}
+}
+
+app();
